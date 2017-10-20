0dca77d666237270989fe3c750cf606bd3859b6f
Tarunyo <32845547+Tarunyo@users.noreply.github.com>
2017-10-20 11:13:05 +0530

    Update huffmanCode.cpp

--- huffmanCode.cpp
+++ huffmanCode.cpp
@@ -1,87 +1,115 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-// A Huffman tree node
-struct MinHeapNode
+#include <iostream>
+#include <queue>
+#include <map>
+#include <climits> // for CHAR_BIT
+#include <iterator>
+#include <algorithm>
+
+const int UniqueSymbols = 1 << CHAR_BIT;
+const char* SampleString = "this is an example for huffman encoding";
+
+typedef std::vector<bool> HuffCode;
+typedef std::map<char, HuffCode> HuffCodeMap;
+
+class INode
 {
-    char data;                // One of the input characters
-    unsigned freq;             // Frequency of the character
-    MinHeapNode *left, *right; // Left and right child
- 
-    MinHeapNode(char data, unsigned freq)
-    {
-        left = right = NULL;
-        this->data = data;
-        this->freq = freq;
-    }
+public:
+	const int f;
+
+	virtual ~INode() {}
+
+protected:
+	INode(int f) : f(f) {}
 };
- 
-// For comparison of two heap nodes (needed in min heap)
-struct compare
+
+class InternalNode : public INode
 {
-    bool operator()(MinHeapNode* l, MinHeapNode* r)
-    {
-        return (l->freq > r->freq);
-    }
+public:
+	INode *const left;
+	INode *const right;
+
+	InternalNode(INode* c0, INode* c1) : INode(c0->f + c1->f), left(c0), right(c1) {}
+	~InternalNode()
+	{
+		delete left;
+		delete right;
+	}
 };
- 
-// Prints huffman codes from the root of Huffman Tree.
-void printCodes(struct MinHeapNode* root, string str)
+
+class LeafNode : public INode
 {
-    if (!root)
-        return;
- 
-    if (root->data != '$')
-        cout << root->data << ": " << str << "\n";
- 
-    printCodes(root->left, str + "0");
-    printCodes(root->right, str + "1");
-}
- 
-// The main function that builds a Huffman Tree and
-// print codes by traversing the built Huffman Tree
-void HuffmanCodes(char data[], int freq[], int size)
+public:
+	const char c;
+
+	LeafNode(int f, char c) : INode(f), c(c) {}
+};
+
+struct NodeCmp
 {
-    struct MinHeapNode *left, *right, *top;
- 
-    // Create a min heap & inserts all characters of data[]
-    priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
-    for (int i = 0; i < size; ++i)
-        minHeap.push(new MinHeapNode(data[i], freq[i]));
- 
-    // Iterate while size of heap doesn't become 1
-    while (minHeap.size() != 1)
-    {
-        // Extract the two minimum freq items from min heap
-        left = minHeap.top();
-        minHeap.pop();
- 
-        right = minHeap.top();
-        minHeap.pop();
- 
-        // Create a new internal node with frequency equal to the
-        // sum of the two nodes frequencies. Make the two extracted
-        // node as left and right children of this new node. Add
-        // this node to the min heap
-        // '$' is a special value for internal nodes, not used
-        top = new MinHeapNode('$', left->freq + right->freq);
-        top->left = left;
-        top->right = right;
-        minHeap.push(top);
-    }
- 
-    // Print Huffman codes using the Huffman tree built above
-    printCodes(minHeap.top(), "");
+	bool operator()(const INode* lhs, const INode* rhs) const { return lhs->f > rhs->f; }
+};
+
+INode* BuildTree(const int (&frequencies)[UniqueSymbols])
+{
+	std::priority_queue<INode*, std::vector<INode*>, NodeCmp> trees;
+
+	for (int i = 0; i < UniqueSymbols; ++i)
+	{
+		if(frequencies[i] != 0)
+		trees.push(new LeafNode(frequencies[i], (char)i));
+	}
+	while (trees.size() > 1)
+	{
+		INode* childR = trees.top();
+		trees.pop();
+
+		INode* childL = trees.top();
+		trees.pop();
+
+		INode* parent = new InternalNode(childR, childL);
+		trees.push(parent);
+	}
+	return trees.top();
 }
- 
-// Driver program to test above functions
+
+void GenerateCodes(const INode* node, const HuffCode& prefix, HuffCodeMap& outCodes)
+{
+	if (const LeafNode* lf = dynamic_cast<const LeafNode*>(node))
+	{
+		outCodes[lf->c] = prefix;
+	}
+	else if (const InternalNode* in = dynamic_cast<const InternalNode*>(node))
+	{
+		HuffCode leftPrefix = prefix;
+		leftPrefix.push_back(false);
+		GenerateCodes(in->left, leftPrefix, outCodes);
+
+		HuffCode rightPrefix = prefix;
+		rightPrefix.push_back(true);
+		GenerateCodes(in->right, rightPrefix, outCodes);
+	}
+}
+
 int main()
 {
-    char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
-    int freq[] = { 5, 9, 12, 13, 16, 45 };
-    int size = sizeof(arr) / sizeof(arr[0]);
- 
-    HuffmanCodes(arr, freq, size);
- 
-    return 0;
+	// Build frequency table
+	int frequencies[UniqueSymbols] = {0};
+	const char* ptr = SampleString;
+	while (*ptr != '\0')
+	++frequencies[*ptr++];
+
+	INode* root = BuildTree(frequencies);
+
+	HuffCodeMap codes;
+	GenerateCodes(root, HuffCode(), codes);
+	delete root;
+
+	for (HuffCodeMap::const_iterator it = codes.begin(); it != codes.end(); ++it)
+	{
+		std::cout << it->first << " ";
+		std::copy(it->second.begin(), it->second.end(),
+		std::ostream_iterator<bool>(std::cout));
+		std::cout << std::endl;
+	}
+	return 0;
 }
